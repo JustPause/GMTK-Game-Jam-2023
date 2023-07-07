c043d5a7da45e2a8841b33d2efa7553646c8986b
Justinas Stankunas (JustPuase) <IamJustStan@hotmail.com>
2023-07-08 02:48:38 +0300

    Movement has been added, it works good

---
+++ GMTK-Game-Jam-2023/Assets/Player/Scripts/PlayerMovment.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+
+    public float movementSpeed = 10;
+    public float jumpPowor = 1;
+    public float gravity = 9.8f;
+
+    public Rigidbody rigidbody;
+    public Vector3 movement;
+
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector3 vector3)
+    {
+        // rigidbody.AddForce(vector3 * (movementSpeed * 100) * Time.deltaTime);
+        rigidbody.velocity = (vector3 * (movementSpeed * 100) * Time.deltaTime);
+    }
+}
