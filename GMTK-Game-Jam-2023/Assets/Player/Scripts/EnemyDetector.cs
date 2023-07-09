c65652fa24c71fa64c5808cf724fbda16b340969
Justinas Stankunas (JustPuase) <IamJustStan@hotmail.com>
2023-07-09 19:50:48 +0300

    Sad

--- GMTK-Game-Jam-2023/Assets/Player/Scripts/EnemyDetector.cs
+++ GMTK-Game-Jam-2023/Assets/Player/Scripts/EnemyDetector.cs
@@ -5,20 +5,66 @@ using UnityEngine;
 
 public class EnemyDetector : MonoBehaviour
 {
-
-    public FolowMainCharecter folowMainCharecter;
+    public Transform target;
+    public LineRenderer lineRenderer;
+    public FolowMainCharecter Target;
+    public Material material;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (folowMainCharecter.distance < 30)
+        if (Target.distance < 30)
         {
-            Debug.Log(folowMainCharecter.distance);
+            lineRenderer.SetPosition(
+                0,
+                new Vector3
+                (
+                transform.position.x,
+                transform.position.y,
+                transform.position.z
+                )
+            );
+
+            lineRenderer.SetPosition(
+                1,
+                new Vector3(
+                target.position.x,
+                target.position.y,
+                target.position.z
+                )
+            );
+            material.SetColor("_EmissionColor", new Vector4(255,0,0,255) * 0.2f);
+
+            Debug.Log(Target.distance);
+        }
+        else
+        {
+            material.color = Color.black;
+            material.SetColor("_EmissionColor", new Vector4(255,255,255,255) * 0);
+
+            lineRenderer.SetPosition(
+                0,
+                new Vector3
+                (
+                transform.position.x,
+                transform.position.y,
+                transform.position.z
+                )
+            );
+
+            lineRenderer.SetPosition(
+                1,
+                new Vector3
+                (
+                target.position.x,
+                target.position.y,
+                target.position.z
+                )
+            );
         }
     }
 }
